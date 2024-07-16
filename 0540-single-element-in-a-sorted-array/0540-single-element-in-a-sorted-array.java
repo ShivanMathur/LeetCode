cc3fddc453d0c7549c620ee72aa47a3a12d0f90e
Shivan Mathur <31898751+Shivan-Mathur@users.noreply.github.com>
2024-07-16 01:12:02 -0400

    Time: 0 ms (100%), Space: 49.8 MB (91.32%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,18 +1,49 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int low = 0, high = nums.length-1;
+        /*
+         * int low = 0, high = nums.length-1;
+         * 
+         * while(low < high){
+         * int mid = low + (high-low)/2;
+         * 
+         * if( (mid%2 == 0 && nums[mid] == nums[mid+1]) || (mid%2 == 1 && nums[mid] ==
+         * nums[mid-1]) ){
+         * low = mid + 1;
+         * }
+         * else{
+         * high = mid;
+         * }
+         * }
+         * 
+         * return nums[low];
+         */
 
-        while(low < high){
-            int mid = low + (high-low)/2;
+        /* TUF Approach */
+        if (nums.length <= 1) {
+            return nums[0];
+        }
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+        if (nums[nums.length - 1] != nums[nums.length - 2]) {
+            return nums[nums.length - 1];
+        }
 
-            if( (mid%2 == 0 && nums[mid] == nums[mid+1]) || (mid%2 == 1 && nums[mid] == nums[mid-1]) ){
-                low = mid + 1;
+        int low = 1, high = nums.length - 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
             }
-            else{
-                high = mid;
+
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
-        return nums[low];
+        return -1;
     }
 }
