74d5321a34b5a8a398c27fefc08b4ce0ecd62236
Shivan Mathur <31898751+Shivan-Mathur@users.noreply.github.com>
2024-07-22 19:33:54 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        if(nums.length < k){
+            return -1;
+        }
+
+        int low = 0, high = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>low){
+                low = nums[i];
+            }
+            high += nums[i];
+        }
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(findSum(nums, mid, k)){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+
+        return low;
+    }
+
+    static boolean findSum(int[] nums, int split, int k) {
+        int arrSize = 1, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > split) {
+                arrSize++;
+                sum = nums[i];
+            }
+        }
+        return (arrSize == k);
+    }
+}
