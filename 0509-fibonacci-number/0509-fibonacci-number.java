36e32f05cc263beb73e125d510d52707ede569ed
Shivan Mathur <31898751+ShivanMathur@users.noreply.github.com>
2024-08-31 02:14:55 -0400

    Time: 0 ms (100%), Space: 39.9 MB (96.99%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,41 @@
 class Solution {
     public int fib(int n) {
-        /* RECURSION Approach */
+        /* RECURSION Approach
         if (n <= 1){
             return n;
         }
 
-        return fib(n-1) + fib(n-2);
+        return fib(n-1) + fib(n-2); */
+
+        /* Memoization Approach
+        int[] dp = new int[n+1];
+        return fib(n, dp); */
+
+        /* Tabulation Approach */
+        if(n <= 1){
+            return n;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+    private int fib(int n, int[] dp){
+        if(n <= 1){
+            return n;
+        }
+
+        if(dp[n] != 0){
+            return dp[n];
+        }
+
+        dp[n] = fib(n-1, dp) + fib(n-2, dp);
+        return dp[n];
     }
 }
