06eb3b80e5a13bb280bd7980ce7984e9b363326e
Shivan Mathur <31898751+ShivanMathur@users.noreply.github.com>
2024-09-02 19:01:58 -0400

    Time: 0 ms (100%), Space: 40.3 MB (99.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,9 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
-        /* DP: Memoization */
+        /* DP: Memoization 
+        TC: O(N)
+        SC: O(N) + O(N)
         int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
-        return rob(nums, nums.length - 1, dp);
+        return rob(nums, nums.length - 1, dp); */
+
+        /* DP: Tabulation */
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+
+        for(int i=1; i<nums.length; i++){
+            int pick = nums[i];
+            if(i>1){
+                pick += dp[i-2];
+            }
+
+            int notpick = dp[i-1];
+
+            dp[i] = Math.max(pick, notpick);
+        }
+
+        return dp[nums.length-1];
     }
 
     private int rob(int[] nums, int index, int[] dp){
