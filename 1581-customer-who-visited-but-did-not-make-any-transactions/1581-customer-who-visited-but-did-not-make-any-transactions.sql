ef1892a469dbb18d428d5b051808946d2a5001f7
Shivan Mathur <shivanmthr18@gmail.com>
2025-05-01 16:41:54 -0400

    Time: 462 ms (89.68%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,15 +1,15 @@
-/* Write your PL/SQL query statement below */
+-- Write your PostgreSQL query statement below
 
-/* Approach 1: Removing records using NOT IN/EXISTS*/
+-- APPROACH 1: Using NOT IN
+-- SELECT customer_id, COUNT(visit_id) AS count_no_trans
+-- FROM visits 
+-- WHERE visit_id NOT IN (SELECT visit_id FROM transactions)
+-- GROUP BY 1;
+
+-- APPROACH 2: Using LEFT JOIN
 SELECT v.customer_id, COUNT(*) AS count_no_trans
 FROM visits v
-WHERE v.visit_id NOT IN (SELECT t.visit_id FROM transactions t)
-GROUP BY v.customer_id; 
-
-/* Approach 2: Removing records using LEFT JOIN and IS NULL
-SELECT v.customer_id, count(*) as count_no_trans
-from visits v
-left outer join transactions t on v.visit_id = t.visit_id
-where t.transaction_id is null
-group by v.customer_id;
-*/
+LEFT JOIN transactions t 
+ON v.visit_id = t.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY 1;
