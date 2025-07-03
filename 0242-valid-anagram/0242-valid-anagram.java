c8e93ba21a013c661a39962a8b84f635b75d7c51
Shivan Mathur <shivanmthr18@gmail.com>
2025-07-03 18:52:14 -0400

    Time: 5 ms (56.68%), Space: 43.3 MB (66.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         // HashMap Approach
+        /* 
         HashMap<Character, Integer> map = new HashMap<>();
         int s_len = s.length();
         int t_len = t.length();
@@ -16,6 +17,29 @@ class Solution {
 
         for(char c:map.keySet()){
             if(map.get(c)!=0){
+                return false;
+            }
+        }
+
+        return true;
+        */
+
+        // 
+        int s_len = s.length();
+        int t_len = t.length();
+
+        if(s_len != t_len){
+            return false;
+        }
+
+        int[] char_frequency = new int[26];
+        for(int i=0; i<s_len; i++){
+            char_frequency[s.charAt(i) - 'a']++;
+            char_frequency[t.charAt(i) - 'a']--;
+        }
+
+        for(int i=0; i<char_frequency.length; i++){
+            if(char_frequency[i]!=0){
                 return false;
             }
         }
