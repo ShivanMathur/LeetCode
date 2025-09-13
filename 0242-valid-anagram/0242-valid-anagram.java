73f8120ccfdb025cac20fc704cdd92c518f7f0e5
Shivan Mathur <shivanmthr18@gmail.com>
2025-09-13 18:18:07 -0400

    Time: 4 ms (76.74%), Space: 44.6 MB (51.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,49 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // HashMap Approach
-        /* 
-        HashMap<Character, Integer> map = new HashMap<>();
-        int s_len = s.length();
-        int t_len = t.length();
-
-        if(s_len != t_len){
-            return false;
-        }
-
-        for(int i=0; i<s_len; i++){
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
-        }
-
-        for(char c:map.keySet()){
-            if(map.get(c)!=0){
-                return false;
-            }
-        }
-
-        return true;
+        /* Approach 1: Sorting both the strings and compare
+            TC: O(nlogn + mlogm)
+            SC: O(1) {Worst case if arrays are of different size} OR O(n){Array element comparison}
         */
 
-        // 
-        int s_len = s.length();
-        int t_len = t.length();
-
-        if(s_len != t_len){
+        if(s.length() != t.length()){
             return false;
         }
 
-        int[] char_frequency = new int[26];
-        for(int i=0; i<s_len; i++){
-            char_frequency[s.charAt(i) - 'a']++;
-            char_frequency[t.charAt(i) - 'a']--;
-        }
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
 
-        for(int i=0; i<char_frequency.length; i++){
-            if(char_frequency[i]!=0){
-                return false;
-            }
-        }
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
 
-        return true;
+        return Arrays.equals(sArray, tArray);
     }
 }
