69b0063569b73e38de8c68806b68be8451e003e3
Shivan Mathur <shivanmthr18@gmail.com>
2025-09-15 01:10:53 -0400

    Time: 5 ms (55.52%), Space: 43.3 MB (67.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,49 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        /* Approach 1: Sorting both the strings and compare
-            TC: O(nlogn + mlogm)
-            SC: O(1) {Worst case if arrays are of different size} OR O(n){Array element comparison}
-        */
+        // HashMap Approach
+        /* 
+        HashMap<Character, Integer> map = new HashMap<>();
+        int s_len = s.length();
+        int t_len = t.length();
 
-        if(s.length() != t.length()){
+        if(s_len != t_len){
             return false;
         }
 
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
+        for(int i=0; i<s_len; i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        }
 
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
+        for(char c:map.keySet()){
+            if(map.get(c)!=0){
+                return false;
+            }
+        }
 
-        return Arrays.equals(sArray, tArray);
+        return true;
+        */
+
+        // 
+        int s_len = s.length();
+        int t_len = t.length();
+
+        if(s_len != t_len){
+            return false;
+        }
+
+        int[] char_frequency = new int[26];
+        for(int i=0; i<s_len; i++){
+            char_frequency[s.charAt(i) - 'a']++;
+            char_frequency[t.charAt(i) - 'a']--;
+        }
+
+        for(int i=0; i<char_frequency.length; i++){
+            if(char_frequency[i]!=0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
