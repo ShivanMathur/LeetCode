b4bcc5cdd6a02095239666d7567da7e4a94bb8e0
Shivan Mathur <shivanmthr18@gmail.com>
2025-09-22 00:27:02 -0400

    Time: 5 ms (88.84%), Space: 44.3 MB (86.03%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,16 +1,45 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int left = 0, maxLength = 0;
-        Map<Character, Integer> count = new HashMap<>();
+        /* 2. Sliding Window (Optimized)
+            TC: O(N)
+            SC: O(M) ; M: Total number of unique characters in the string 
+        */
 
-        for(int right = 0; right<s.length(); right++){
-            char c = s.charAt(right);
-            if(count.containsKey(c)){
-                left = Math.max(left, count.get(c));
+        int maxLength = 0;
+        int left = 0, right = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while(right < s.length()){
+            if(map.containsKey(s.charAt(right))){
+                left = Math.max(left, map.get(s.charAt(right)) + 1);
             }
-            maxLength = Math.max(maxLength, (right - left) + 1);
-            count.put(c, right + 1);
+            map.put(s.charAt(right), right);
+            maxLength = Math.max(maxLength, right - left + 1);
+            right++;
         }
+
         return maxLength;
+        
+        /* 1. Sliding Window
+            TC: O(N)
+            SC: O(M) ; M: Total number of unique characters in the string 
+
+        int left = 0, right = 0;
+        Set<Character> set = new HashSet<>();
+
+        int maxLength = 0;
+
+        while(right < s.length()){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+            right++;
+        }
+
+        return maxLength;
+        */
     }
 }
