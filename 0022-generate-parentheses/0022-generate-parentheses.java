2f3835b505b14ca8176eb4d1de92daee6edb3250
Shivan Mathur <shivanmthr18@gmail.com>
2025-09-28 00:58:41 -0400

    Time: 1 ms (80.69%), Space: 43.3 MB (45.22%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,5 +1,43 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
+        /* Recursion: Better Approach
+            TC: O(2^2n)
+            SC: O(2n)
+        
+        Intuition: If the openCount (open bracket count) goes beyond N or the closeCount is greater than openCount, then no need to generate further. It will always be invalid.
+
+        Otherwise, it will be valid string.
+        */
+
+        int openCount = 0;
+        int closeCount = 0;
+        List<String> result = new ArrayList<>();
+
+        backtrack(result, "", openCount, closeCount, n);
+
+        return result;
+    }
+
+    public void backtrack(List<String> result, String str, int openCount, int closeCount, int n){
+        if(str.length() == 2*n){
+            result.add(str);
+            return;
+        }
+
+        if(openCount < n){
+            backtrack(result, str + "(", openCount + 1, closeCount, n);
+        }
+
+        if(closeCount < openCount){
+            backtrack(result, str + ")", openCount, closeCount + 1, n);
+        }
+    }
+
+    /* Recursion Approach
+        TC: O(2^2n * 2n)
+        SC: O(2n) {Depth of Recursion Tree}
+    
+    public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
         backtrack(result, "", n);
         return result;
@@ -33,4 +71,6 @@ class Solution {
 
         return count == 0;
     }
+
+    */
 }
