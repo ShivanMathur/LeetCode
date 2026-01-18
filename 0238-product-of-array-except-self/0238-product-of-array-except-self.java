c4405e5e9376739ced99bc45da7506e0ab310e95
Shivan Mathur <shivanmthr18@gmail.com>
2026-01-17 22:29:08 -0800

    Time: 2 ms (94.32%), Space: 72.2 MB (32.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,54 +1,58 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        /* Prefix and Suffix Product Array Approach (Optimal; without extra space)
+        
+        /* Approach 2: Prefix and Suffix Product Array Approach (Optimal; without extra space)
             TC: O(N)
             SC: O(1)
         */
 
         int n = nums.length;
-        int[] ans = new int[n];
 
-        // Computing Prefix Product
-        ans[0] = 1;
-        for(int i=1; i<n; i++){
-            ans[i] = ans[i-1] * nums[i-1];
-        }
-
-        int postfix = 1;
-        for(int i=n-1; i>=0; i--){
-            ans[i] = ans[i] * postfix;
-            postfix = postfix * nums[i];
-        }
-
-        return ans;
-        
-        /* Prefix and Suffix Product Array Approach
-            TC: O(N)
-            SC: O(N)
-
-        int n = nums.length;
-
-        int[] ans = new int[n];
-        int[] prefix = new int[n];
-        int[] postfix = new int[n];
+        int[] result = new int[n];
 
         // Compute Prefix Product
-        prefix[0] = 1;
+        result[0] = 1;
         for(int i=1; i<n; i++){
-            prefix[i] = prefix[i-1] * nums[i-1];
+            result[i] = result[i-1] * nums[i-1];
         }
 
-        // Compute Postfix Product
-        postfix[n-1] = 1;
+        int suffix = 1;
+        for(int i=n-1; i>=0; i--){
+            result[i] = result[i] * suffix;
+            suffix = suffix * nums[i]; 
+        }
+
+        return result;
+
+        
+        /* Approach 1: Using Prefix and Suffix Product Arrays
+            TC: O(n)
+            SC: O(n)
+
+        
+        int n = nums.length;
+
+        // Computing Prefix Product
+        int[] prefixProduct = new int[n];
+        prefixProduct[0] = 1;
+        for(int i=1; i<n; i++){
+            prefixProduct[i] = prefixProduct[i-1] * nums[i-1];
+        }
+
+        // Computing Suffix Product
+        int[] suffixProduct = new int[n];
+        suffixProduct[n-1] = 1;
         for(int i=n-2; i>=0; i--){
-            postfix[i] = nums[i+1] * postfix[i+1];
+            suffixProduct[i] = suffixProduct[i+1] * nums[i+1];
         }
 
+        // Computing resultant array
+        int[] result = new int[n];
         for(int i=0; i<n; i++){
-            ans[i] = prefix[i] * postfix[i];
+            result[i] = prefixProduct[i] * suffixProduct[i];
         }
 
-        return ans;
-        */  
+        return result;
+        */
     }
 }
