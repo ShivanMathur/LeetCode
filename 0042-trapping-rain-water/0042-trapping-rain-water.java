000df5ab471b76918f8ea5c12489e0eb0837eaf7
Shivan Mathur <shivanmthr18@gmail.com>
2026-01-20 00:31:03 -0800

    Time: 0 ms (100%), Space: 47.8 MB (61.92%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,82 +1,77 @@
 class Solution {
     public int trap(int[] height) {
+        
+        /* Approach 2: Optimized: Two Pointer
+            TC: O(n)
+            SC: O(1)
+        */
 
-        /* TUF:: Better Approach
-            TC: O(N) + O(N) + O(N) = O(3N) {O(2N) for computing prefixMax and suffixMax & O(N) for iterating over the array to calculate the result}
+        int n = height.length;
+        int leftPtr = 0;
+        int rightPtr = n-1;
 
-            SC: O(2N) {Storing prefixMax and suffixMax}
+        int leftMax = 0;
+        int rightMax = 0;
+        
+        int total = 0;
+
+        while(leftPtr < rightPtr){
+            if(height[leftPtr] <= height[rightPtr]){
+                if(height[leftPtr] < leftMax){
+                    total += leftMax - height[leftPtr];
+                }
+                else{
+                    leftMax = height[leftPtr];
+                }
+                leftPtr++;
+            }
+            else{
+                if(height[rightPtr] < rightMax){
+                    total += rightMax - height[rightPtr];
+                }
+                else{
+                    rightMax = height[rightPtr];
+                }
+                rightPtr--;
+            }
+        }
+
+        return total;
+        
+        /* Approach 1: Better Approach
+            TC: O(N) + O(N) + O(N) = O(3N) 
+                { O(2N) for computing prefixMax and suffixMax & 
+                  O(N) for iterating over the array to calculate the result
+                }
+
+            SC: O(2N) {Storing prefixMax and suffixMax} 
+
 
         int n = height.length;
 
         int[] prefixMax = new int[n];
         int[] suffixMax = new int[n];
-        
+
         // Compute prefixMax: Max height towards the left side of the current building
         prefixMax[0] = height[0];
         for(int i=1; i<n; i++){
-            if(height[i] > prefixMax[i-1]){
-                prefixMax[i] = height[i];
-            }
-            else{
-                prefixMax[i] = prefixMax[i-1];
-            }
+            prefixMax[i] = Math.max(prefixMax[i-1], height[i]);
         }
 
         // Compute suffixMax: Max height towards the right side of the current building
-        suffixMax[n - 1] = height[n - 1];
+        suffixMax[n-1] = height[n-1];
         for(int i=n-2; i>=0; i--){
-            if(height[i] > suffixMax[i+1]){
-                suffixMax[i] = height[i];
-            }
-            else{
-                suffixMax[i] = suffixMax[i+1];
-            }
+            suffixMax[i] = Math.max(suffixMax[i+1], height[i]);
         }
 
         // Calculate the water trapped
-        int result = 0;
+        int total = 0;
+
         for(int i=0; i<n; i++){
-            result += Math.min(prefixMax[i], suffixMax[i]) - height[i];
+            total += Math.min(prefixMax[i], suffixMax[i]) - height[i];
         }
 
-        return result;
+        return total;
         */
-
-        /* Optimized Approach: Two-pointer
-        TC: O(N)
-        SC: (1)
-        */
-
-        int n = height.length;
-
-        int leftMax = 0;
-        int rightMax = 0;
-        int l = 0;
-        int r = n-1;
-
-        int result = 0;
-
-        while(l < r){
-            if(height[l] <= height[r]){
-                if(height[l] >= leftMax){
-                    leftMax = height[l];
-                }
-                else{
-                    result += leftMax - height[l];
-                }
-                l++;
-            }
-            else{
-                if(height[r] >= rightMax){
-                    rightMax = height[r];
-                }
-                else{
-                    result += rightMax - height[r];
-                }
-                r--;
-            }
-        } 
-
-        return result;
     }
 }
