b0511e42210d90643285e268d01e1353c92589a7
Shivan Mathur <shivanmthr18@gmail.com>
2026-01-19 23:39:25 -0800

    Time: 36 ms (23.87%), Space: 95.9 MB (22.92%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,53 +1,69 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        /* Better Approach: First Sort the array and then iterate over the array to find the LCS
-        if(nums.length <= 0){
-            return 0;
-        }
-        Arrays.sort(nums);
 
-        int count = 1, longestCount = 0;
+        /* Approach 2: Optimized Approach
+            TC: O(n)
+            SC: O(n)
+        */
 
-        for(int i=1; i<nums.length; i++){
-            if(nums[i] != nums[i-1]){
-                if(nums[i] == nums[i-1]+1 ){
-                    count++;
-                }
-                else{
-                longestCount = Math.max(longestCount, count);
-                count = 1;
-                }
-            }
-        }
-        return Math.max(longestCount, count); */
-
-        /* Optimized Approach */
         if(nums.length <= 0){
             return 0;
         }
 
         HashSet<Integer> set = new HashSet<>();
 
-        // Insert all the array elements into the hashset
-        for(int i : nums){
-            set.add(i);
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
         }
 
         int longestCount = 1;
 
-        // Iterate though the array element
-        for(int num : set){
-            if(!set.contains(num-1)){
-                int count = 1;
-                int element = num;
-                while(set.contains(element + 1)){
-                    count++;
-                    element += 1;
-                }
-                longestCount = Math.max(longestCount, count);
+        for(int key : set){
+            int prevNum = key - 1;
+            if(!set.contains(prevNum)){
+                int currCount = 1;
+                int nextNum = key;
+                while(set.contains(nextNum + 1)){
+                    nextNum += 1;
+                    currCount++;
+                } 
+                longestCount = Math.max(longestCount, currCount);
             }
         }
 
         return longestCount;
+        
+        /* Approach 1: Better Approach: First Sort the array and then iterate to find LCS.
+            TC: O(nlogn) {Sorting} + O(n){traverse}
+            SC: O(1)
+
+
+        if(nums.length <= 0){
+            return 0;
+        }
+        
+        Arrays.sort(nums);
+
+        int lastNum = Integer.MIN_VALUE;
+        int currCount = 0; // Counter for the current consecutive sequence length.
+        int longestCount = 1; // Variable to store the maximum consecutive sequence length.
+
+        for(int i=0; i<nums.length; i++){
+            // If the current number is consecutive to the previous one, increment the counter.
+            if(nums[i] - 1 == lastNum){
+                currCount++;
+                lastNum = nums[i];
+            }
+            // If the current number is not consecutive, update 'currCount' and 'lastNum'.
+            else if(nums[i] != lastNum){
+                lastNum = nums[i];
+                currCount = 1;
+            }
+            // Update 'longestCount' with the maximum of 'longestCount' and 'currCount'.
+            longestCount = Math.max(longestCount, currCount);
+        }
+
+        return longestCount;
+        */
     }
 }
