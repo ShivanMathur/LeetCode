6bb6e23b5b5bebc5b8621c84a17baf4b6de0969b
Shivan Mathur <shivanmthr18@gmail.com>
2026-01-21 01:12:41 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        /* Approach 1: Mathslong Division
+            TC: O()
+            SC: O()
+        */
+
+        if(numerator == 0){
+            return "0";
+        }
+
+        String sign = (numerator * denominator < 0) ? "-" : "";
+
+        long num = (long)numerator;
+        long denom = (long)denominator;
+
+        long res = Math.abs(num / denom);
+        
+        String result = sign + Long.toString(res);
+        
+        long remainder = num % denom;
+
+        if(remainder == 0){
+            return result;
+        }
+
+        HashMap<Long, Integer> map = new HashMap<>();
+        result += ".";
+
+        while(remainder != 0){
+            if(map.containsKey(remainder)){
+                result = result.substring(0, map.get(remainder)) + "(" + result.substring(map.get(remainder)) + ")";
+                break;
+            }
+            map.put(remainder, result.length());
+            remainder = remainder*10;
+            result += Long.toString(remainder / denom);
+            remainder = remainder % denom;
+        }
+
+        return result;
+    }
+}
