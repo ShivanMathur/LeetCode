de1531b296e8b965fda4d224c3f8b99d0bc65af4
Shivan Mathur <shivanmthr18@gmail.com>
2026-01-21 01:15:28 -0800

    Time: 5 ms (4.96%), Space: 43.8 MB (7.33%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -9,10 +9,10 @@ class Solution {
             return "0";
         }
 
-        String sign = (numerator * denominator < 0) ? "-" : "";
+        String sign = (numerator < 0) ^ (denominator < 0) ? "-" : "";
 
-        long num = (long)numerator;
-        long denom = (long)denominator;
+        long num = Math.abs((long)numerator);
+        long denom = Math.abs((long)denominator);
 
         long res = Math.abs(num / denom);
         
