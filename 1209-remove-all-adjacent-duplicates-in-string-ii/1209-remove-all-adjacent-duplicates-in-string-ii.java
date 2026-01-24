6b485d03f4a82fc74c5ddf0de8f8ce696bde4608
Shivan Mathur <shivanmthr18@gmail.com>
2026-01-24 00:45:14 -0800

    Time: 30 ms (52.19%), Space: 47.2 MB (63.97%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,66 @@
+class Solution {
+    public String removeDuplicates(String s, int k) {
+
+        /* Approach 2: Using 1 Stack
+            TC: O(n)
+            SC: O(n)
+        */
+
+        Stack<int[]> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek()[0] == ch){
+                stack.peek()[1]++;
+            }
+            else{
+                stack.push(new int[]{ch, 1});
+            }
+
+            if(stack.peek()[1] == k){
+                stack.pop();
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while(!stack.isEmpty()){
+            int[] top = stack.pop();
+            while(top[1]-- > 0){
+                result.append((char)top[0]);
+            }
+        }
+
+        return result.reverse().toString();
+        
+        /* Approach 1: Using 2 Stacks -- Time Limit Exceeded
+            TC: O(n * k)
+            SC: O(n)
+        
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            Stack<Character> tempStack = new Stack<>();
+
+            tempStack.push(ch);
+
+            while(!stack.isEmpty() && stack.peek() == tempStack.peek()){
+                tempStack.push(stack.pop());
+            }
+            if(tempStack.size() != k){
+                while(!tempStack.isEmpty()){
+                    stack.push(tempStack.pop());
+                }
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while(!stack.isEmpty()){
+            result.append(stack.pop());
+        }
+
+        return result.reverse().toString();
+        */
+    }
+}
