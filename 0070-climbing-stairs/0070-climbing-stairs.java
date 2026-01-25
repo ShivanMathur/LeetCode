26df998f0d62a41b3f5c1d008545f1c4bc2666b1
Shivan Mathur <shivanmthr18@gmail.com>
2026-01-24 20:20:24 -0800

    Time: 0 ms (100%), Space: 42.2 MB (25.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,62 @@
 class Solution {
+    
+    private int[] dpMemo;
+
     public int climbStairs(int n) {
 
-        /* DP: TABULATION APPROACH */
+        /* Approach 3: DP - Tabulation
+            TC: O(n)
+            SC: O(n)
+        */
 
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            if (i == 1) {
-                dp[i] = dp[i - 1];
-            } else{
-                dp[i] = dp[i - 1] + dp[i - 2];
-            }
+        if(n <= 1){
+            return 1;
         }
 
-        return dp[n];
+        int[] dpTab = new int[n+1];
+        dpTab[0] = 1;
+        dpTab[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dpTab[i] = dpTab[i-1] + dpTab[i-2];
+        }
+
+        return dpTab[n];
+
+        
+        
+        /* Approach 2: DP - Memoization
+            TC: O(n)
+            SC: O(n)
+        
+        dpMemo = new int[n+1];
+        return climbStairsDPMemoization(n);
+        
+        */
+
+        
+        /* Approach 1: Recursion
+            TC: O(2^n)
+            SC: O(n)
+
+        if(n <= 1){
+            return 1;
+        }
+
+        return climbStairs(n - 1) + climbStairs(n - 2);
+        */
+    }
+
+    private int climbStairsDPMemoization(int n){
+        if(n <= 1){
+            dpMemo[n] = 1;
+        }
+
+        if(dpMemo[n] != 0){
+            return dpMemo[n];
+        }
+
+        dpMemo[n] = climbStairsDPMemoization(n-1) + climbStairsDPMemoization(n-2);
+        return dpMemo[n];
     }
 }
