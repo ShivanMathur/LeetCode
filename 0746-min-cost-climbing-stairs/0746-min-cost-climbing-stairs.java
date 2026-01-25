eca8690279d31f60735b2afb223909ac021e99e7
Shivan Mathur <shivanmthr18@gmail.com>
2026-01-24 21:04:24 -0800

    Time: 0 ms (100%), Space: 45 MB (50.02%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,89 +1,77 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        /* Recursion: 
-        TC: O(2^n) 
-        int n = cost.length;
-        int opt1 = minCostClimbingStairsHelper_Recursion(cost, n-1);
-        int opt2 = minCostClimbingStairsHelper_Recursion(cost, n-2);
 
-        return Math.min(opt1, opt2); */
-
-        /* DP: Memoization Approach
-        TC: O(N)
+        /* Approach 3: DP - Tabulation
+            TC: O(n)
+            SC: O(n)
+        */
 
         int n = cost.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-
-        int opt1 = minCostClimbingStairsHelper(cost, n-1, dp);
-        int opt2 = minCostClimbingStairsHelper(cost, n-2, dp);
-
-        return Math.min(opt1, opt2); */
-
-        /* DP: Tabulation Approach
-        TC: O(N)
-        SC: O(N)
-        
-        int n = cost.length;
-        int[] dp = new int[n];
+        int[] dpTab = new int[n];
 
         for(int i=0; i<n; i++){
             if(i < 2){
-                dp[i] = cost[i];
+                dpTab[i] = cost[i];
             }
             else{
-                dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+                dpTab[i] = cost[i] + Math.min(dpTab[i-1], dpTab[i-2]);
             }
         }
 
-        return Math.min(dp[n-1], dp[n-2]); */
+        return Math.min(dpTab[n-1], dpTab[n-2]);
 
-        /* DP: Space Optimization 
-        TC: O(N)
-        SC: O(1) */
+        /* Approach 2: DP - Memoization
+            TC: O(n)
+            SC: O(n)
         
-        int n = cost.length;
-        int prev2 = cost[0];
-        int prev1 = cost[1];
 
-        for(int i=2; i<n; i++){
-            int curr = cost[i] + Math.min(prev1, prev2);
-            prev2 = prev1;
-            prev1 = curr;
-        }
+        int[] dpMemo = new int[cost.length + 1];
+        Arrays.fill(dpMemo, -1);
 
-        return Math.min(prev2, prev1);
+        int option_step_0 = minCostClimbingStairsDPMemoization(0, cost, dpMemo);
+        int option_step_1 = minCostClimbingStairsDPMemoization(1, cost, dpMemo);
+
+        return Math.min(option_step_0,  option_step_1);
+
+        */
+
+        
+        /* Approach 1: Recursion
+            TC: O(2^n)
+            SC: O(n)
+        
+
+        int option_step_0 = minCostClimbingStairsRecursion(0, cost);
+        int option_step_1 = minCostClimbingStairsRecursion(1, cost);
+
+        return Math.min(option_step_0, option_step_1);
+        */
     }
 
-    // DP Memoization Helper Function
-    private int minCostClimbingStairsHelper(int[] cost, int start, int[] dp){
-        if(start < 0){
+    private int minCostClimbingStairsDPMemoization(int index, int[] cost, int[] dpMemo){
+        if(index >= cost.length){
             return 0;
         }
-        if(start == 0 || start == 1){
-            return cost[start];
+
+        if(dpMemo[index] != -1){
+            return dpMemo[index];
         }
 
-        if(dp[start] != -1){
-            return dp[start];
-        }
-        int oneStep = minCostClimbingStairsHelper(cost, start-1, dp);
-        int twoStep = minCostClimbingStairsHelper(cost, start-2, dp);
+        int oneStep = cost[index] + minCostClimbingStairsDPMemoization(index + 1, cost, dpMemo);
+        int twoStep = cost[index] + minCostClimbingStairsDPMemoization(index + 2, cost, dpMemo);
 
-        return dp[start] = cost[start] + Math.min(oneStep, twoStep);
+        dpMemo[index] = Math.min(oneStep, twoStep);
+        return dpMemo[index];
     }
 
-    // Recursion Helper Function
-    private int minCostClimbingStairsHelper_Recursion(int[] cost, int start){
-        if(start < 0){
+    private int minCostClimbingStairsRecursion(int n, int[] cost){
+        if( n >= cost.length){
             return 0;
         }
-        if(start == 0 || start == 1){
-            return cost[start];
-        }
-        int oneStep = minCostClimbingStairsHelper_Recursion(cost, start-1);
-        int twoStep = minCostClimbingStairsHelper_Recursion(cost, start-2);
 
-        return cost[start] + Math.min(oneStep, twoStep);
+        int oneStep = minCostClimbingStairsRecursion(n+1, cost);
+        int twoStep = minCostClimbingStairsRecursion(n+2, cost);
+
+        return cost[n] + Math.min(oneStep, twoStep);
     }
 }
